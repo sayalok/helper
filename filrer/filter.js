22715bcb211d82b88d388eb7ea8d481a8c325b1f
Faisal <38526562+sayalok@users.noreply.github.com>
2020-11-25 19:56:12 +0600

    Add files via upload

---
+++ filrer/filter.js
@@ -0,0 +1,419 @@
+const data = {
+    "status_code":200,
+    "message":"Successful",
+    "departureScheduleData":{
+       "status":1,
+       "scheduleData":[
+          {
+             "GRL_36097":{
+                "uid":"0SSI0HM",
+                "transport_type":"1",
+                "transport_id":"37",
+                "bus_service_type":"GRL",
+                "departure_id":36097,
+                "id":"72",
+                "bus_id":"72",
+                "sku":"37",
+                "seat_types":[
+                   "ECONOMY"
+                ],
+                "fares":{
+                   "ECONOMY":1000
+                },
+                "coach_no":"509-E-GAB",
+                "coach_type":"Non-Ac",
+                "company_name":"Green Line",
+                "route_name":"Dhaka-Sylhet",
+                "start_counter":"Kalabagan1",
+                "end_counter":"ARAMBAGH-4",
+                "departure_date":"2020-11-25",
+                "departure_time":"00:05",
+                "is_ticket_cancelable":1,
+                "departingTime":"Night"
+             },
+             "GRL_37609":{
+                "uid":"0SSI0HM",
+                "transport_type":"1",
+                "transport_id":"37",
+                "bus_service_type":"GRL",
+                "departure_id":37609,
+                "id":"86",
+                "bus_id":"86",
+                "sku":"37",
+                "seat_types":[
+                   "DOUBLE DECKER-UPPER"
+                ],
+                "fares":{
+                   "DOUBLE DECKER-UPPER":1200
+                },
+                "coach_no":"510-K\/B-DD UPPER DECK",
+                "coach_type":"Ac",
+                "company_name":"Green Line",
+                "route_name":"Dhaka-Sylhet",
+                "start_counter":"Razarbagh1",
+                "end_counter":"ARAMBAGH-4",
+                "departure_date":"2020-11-25",
+                "departure_time":"00:10",
+                "is_ticket_cancelable":1,
+                "departingTime":"Night"
+             },
+             "GRL_37610":{
+                "uid":"0SSI0HM",
+                "transport_type":"1",
+                "transport_id":"37",
+                "bus_service_type":"GRL",
+                "departure_id":37610,
+                "id":"85",
+                "bus_id":"85",
+                "sku":"37",
+                "seat_types":[
+                   "DOUBLE DECKER-LOWER"
+                ],
+                "fares":{
+                   "DOUBLE DECKER-LOWER":1200
+                },
+                "coach_no":"510-K\/B-DD LOWER DECK",
+                "coach_type":"Non-Ac",
+                "company_name":"Green Line",
+                "route_name":"Dhaka-Sylhet",
+                "start_counter":"Razarbagh1",
+                "end_counter":"ARAMBAGH-4",
+                "departure_date":"2020-11-25",
+                "departure_time":"00:10",
+                "is_ticket_cancelable":1,
+                "departingTime":"Night"
+             },
+             "GRL_36372":{
+                "uid":"0SSI0HM",
+                "transport_type":"1",
+                "transport_id":"37",
+                "bus_service_type":"GRL",
+                "departure_id":36372,
+                "id":"72",
+                "bus_id":"72",
+                "sku":"37",
+                "seat_types":[
+                   "ECONOMY"
+                ],
+                "fares":{
+                   "ECONOMY":1000
+                },
+                "coach_no":"501-E-K\/B",
+                "coach_type":"Ac",
+                "company_name":"Green Line",
+                "route_name":"Dhaka-Sylhet",
+                "start_counter":"Kalabagan1",
+                "end_counter":"",
+                "departure_date":"2020-11-25",
+                "departure_time":"07:05",
+                "is_ticket_cancelable":1,
+                "departingTime":"Morning"
+             },
+             "GRL_37604":{
+                "uid":"0SSI0HM",
+                "transport_type":"1",
+                "transport_id":"37",
+                "bus_service_type":"GRL",
+                "departure_id":37604,
+                "id":"86",
+                "bus_id":"86",
+                "sku":"37",
+                "seat_types":[
+                   "DOUBLE DECKER-UPPER"
+                ],
+                "fares":{
+                   "DOUBLE DECKER-UPPER":1200
+                },
+                "coach_no":"502-R\/B-DD-UPPER DECK",
+                "coach_type":"Non-Ac",
+                "company_name":"Green Line",
+                "route_name":"Dhaka-Sylhet",
+                "start_counter":"Razarbagh1",
+                "end_counter":"ARAMBAGH-4",
+                "departure_date":"2020-11-25",
+                "departure_time":"09:15",
+                "is_ticket_cancelable":1,
+                "departingTime":"Morning"
+             },
+             "GRL_37605":{
+                "uid":"0SSI0HM",
+                "transport_type":"1",
+                "transport_id":"37",
+                "bus_service_type":"GRL",
+                "departure_id":37605,
+                "id":"85",
+                "bus_id":"85",
+                "sku":"37",
+                "seat_types":[
+                   "DOUBLE DECKER-LOWER"
+                ],
+                "fares":{
+                   "DOUBLE DECKER-LOWER":1200
+                },
+                "coach_no":"502-R\/B-DD-LOWER DECK",
+                "coach_type":"Ac",
+                "company_name":"Green Line",
+                "route_name":"Dhaka-Sylhet",
+                "start_counter":"Razarbagh1",
+                "end_counter":"ARAMBAGH-4",
+                "departure_date":"2020-11-25",
+                "departure_time":"09:15",
+                "is_ticket_cancelable":1,
+                "departingTime":"Morning"
+             },
+             "GRL_36094":{
+                "uid":"0SSI0HM",
+                "transport_type":"1",
+                "transport_id":"37",
+                "bus_service_type":"GRL",
+                "departure_id":36094,
+                "id":"71",
+                "bus_id":"71",
+                "sku":"37",
+                "seat_types":[
+                   "BUSINESS"
+                ],
+                "fares":{
+                   "BUSINESS":1200
+                },
+                "coach_no":"502-B-R\/B",
+                "coach_type":"Ac",
+                "company_name":"Green Line",
+                "route_name":"Dhaka-Sylhet",
+                "start_counter":"Razarbagh1",
+                "end_counter":"",
+                "departure_date":"2020-11-25",
+                "departure_time":"09:30",
+                "is_ticket_cancelable":1,
+                "departingTime":"Morning"
+             },
+             "GRL_36095":{
+                "uid":"0SSI0HM",
+                "transport_type":"1",
+                "transport_id":"37",
+                "bus_service_type":"GRL",
+                "departure_id":36095,
+                "id":"72",
+                "bus_id":"72",
+                "sku":"37",
+                "seat_types":[
+                   "ECONOMY"
+                ],
+                "fares":{
+                   "ECONOMY":1000
+                },
+                "coach_no":"504-E-R\/B",
+                "coach_type":"Ac",
+                "company_name":"Green Line",
+                "route_name":"Dhaka-Sylhet",
+                "start_counter":"Razarbagh1",
+                "end_counter":"",
+                "departure_date":"2020-11-25",
+                "departure_time":"14:30",
+                "is_ticket_cancelable":1,
+                "departingTime":"After Noon"
+             },
+             "GRL_36096":{
+                "uid":"0SSI0HM",
+                "transport_type":"1",
+                "transport_id":"37",
+                "bus_service_type":"GRL",
+                "departure_id":36096,
+                "id":"71",
+                "bus_id":"71",
+                "sku":"37",
+                "seat_types":[
+                   "BUSINESS"
+                ],
+                "fares":{
+                   "BUSINESS":1200
+                },
+                "coach_no":"505-B-R\/B",
+                "coach_type":"Non-Ac",
+                "company_name":"Green Line",
+                "route_name":"Dhaka-Sylhet",
+                "start_counter":"Razarbagh1",
+                "end_counter":"",
+                "departure_date":"2020-11-25",
+                "departure_time":"17:05",
+                "is_ticket_cancelable":1,
+                "departingTime":"After Noon"
+             },
+             "GRL_37608":{
+                "uid":"0SSI0HM",
+                "transport_type":"1",
+                "transport_id":"37",
+                "bus_service_type":"GRL",
+                "departure_id":37608,
+                "id":"85",
+                "bus_id":"85",
+                "sku":"37",
+                "seat_types":[
+                   "DOUBLE DECKER-LOWER"
+                ],
+                "fares":{
+                   "DOUBLE DECKER-LOWER":1200
+                },
+                "coach_no":"505-R\/B-DD-LOWER DECK",
+                "coach_type":"Ac",
+                "company_name":"Green Line",
+                "route_name":"Dhaka-Sylhet",
+                "start_counter":"Razarbagh1",
+                "end_counter":"ARAMBAGH-4",
+                "departure_date":"2020-11-25",
+                "departure_time":"17:15",
+                "is_ticket_cancelable":1,
+                "departingTime":"After Noon"
+             },
+             "GRL_37607":{
+                "uid":"0SSI0HM",
+                "transport_type":"1",
+                "transport_id":"37",
+                "bus_service_type":"GRL",
+                "departure_id":37607,
+                "id":"86",
+                "bus_id":"86",
+                "sku":"37",
+                "seat_types":[
+                   "DOUBLE DECKER-UPPER"
+                ],
+                "fares":{
+                   "DOUBLE DECKER-UPPER":1200
+                },
+                "coach_no":"505-R\/B-DD-UPPER DECK",
+                "coach_type":"Non-Ac",
+                "company_name":"Green Line",
+                "route_name":"Dhaka-Sylhet",
+                "start_counter":"Razarbagh1",
+                "end_counter":"ARAMBAGH-4",
+                "departure_date":"2020-11-25",
+                "departure_time":"17:15",
+                "is_ticket_cancelable":1,
+                "departingTime":"After Noon"
+             },
+             "BBS_14559":{
+                "uid":"LNNIOU2",
+                "transport_type":"1",
+                "transport_id":"6eda84d85a0841592a5e366a8bd7c91a",
+                "bus_service_type":"BBS",
+                "departure_id":14559,
+                "id":14559,
+                "bus_id":14559,
+                "sku":"4dd208eaa0348dfbb347a3d13e8ee2f4",
+                "seat_types":[
+                   "E-Class"
+                ],
+                "fares":{
+                   "E-Class":"600.00"
+                },
+                "coach_no":"100-DHK-SYHT",
+                "coach_type":"Ac",
+                "company_name":"Khadiza VIP Service",
+                "route_name":"Dhaka->Sylhet",
+                "start_counter":"ARAMBAGH Counter ( OwN )",
+                "end_counter":"kodomtoli counter",
+                "departure_date":"2020-11-25",
+                "departure_time":"08:00",
+                "is_ticket_cancelable":0,
+                "departingTime":"Morning"
+             },
+             "BBS_14561":{
+                "uid":"LNNIOU2",
+                "transport_type":"1",
+                "transport_id":"6eda84d85a0841592a5e366a8bd7c91a",
+                "bus_service_type":"BBS",
+                "departure_id":14561,
+                "id":14561,
+                "bus_id":14561,
+                "sku":"b8ed0a6dd4f3c00fc81fbb5e814526af",
+                "seat_types":[
+                   "E-Class"
+                ],
+                "fares":{
+                   "E-Class":"600.00"
+                },
+                "coach_no":"101-DHK-SYHT",
+                "coach_type":"Ac",
+                "company_name":"Khadiza VIP Service",
+                "route_name":"Dhaka->Sylhet",
+                "start_counter":"ARAMBAGH Counter ( OwN )",
+                "end_counter":"kodomtoli counter",
+                "departure_date":"2020-11-25",
+                "departure_time":"23:30",
+                "is_ticket_cancelable":0,
+                "departingTime":"Night"
+             }
+          }
+       ]
+    },
+    "reternschedule":{
+       "status":0,
+       "scheduleData":[
+          
+       ]
+    },
+    "extraCharge":20
+}
+
+const preFilterData = data.departureScheduleData.scheduleData[0]
+// console.log(preFilterData)
+
+var resultArr = [];
+let coach_type = "All"
+let departingTime = "Night"
+let price = "LowToHigh"
+
+for (const key in preFilterData) {
+    if (coach_type.trim() != "All" && departingTime.trim() != "All") {
+        preFilterData[key].coach_type === coach_type && preFilterData[key].departingTime === departingTime 
+            ? resultArr.push({[key]: preFilterData[key]}) 
+            : null
+    } else if (coach_type.trim() != "All" && departingTime.trim() == "All") {
+        preFilterData[key].coach_type === coach_type 
+            ? resultArr.push({[key]: preFilterData[key]}) 
+            : null
+    } else if (coach_type.trim() == "All" && departingTime.trim() != "All") {
+        preFilterData[key].departingTime === departingTime 
+            ? resultArr.push({[key]: preFilterData[key]}) 
+            : null
+    } else {
+        resultArr.push(preFilterData[key])
+    }
+}
+// console.log(resultArr)
+// price filter
+for (key in resultArr) {
+    // console.log(resultArr)
+    resultArr.sort(function(a,b) {
+        if (price == "LowToHigh") {
+            return parseInt(Object.values(Object.values(a)[0].fares)[0]) - parseInt(Object.values(Object.values(b)[0].fares)[0])
+        } else if (price == "HighToLow")  {
+            return parseInt(Object.values(Object.values(a)[0].fares)[0]) - parseInt(Object.values(Object.values(b)[0].fares)[0])
+        }
+    })
+}
+
+console.log(resultArr)
+
+// if (coach_type != null && coach_type != 'All') {
+//     let coatchTypeArr = []
+//     for(ct in preFilterData) {
+//         preFilterData[ct].coach_type === coach_type ? coatchTypeArr.push(preFilterData[ct]) : null
+//     }
+//     resultArr = coatchTypeArr
+// }
+
+// if (departingTime != null && departingTime != 'All') {
+//     let depatureTimeArr = []
+//     for(dt in resultArr) {
+//         resultArr[dt].departingTime === departingTime ? depatureTimeArr.push(resultArr[dt]) : null
+//     }
+//     resultArr = depatureTimeArr
+// }
+
+// for (key in resultArr) {
+//     resultArr.sort(function(a,b) {
+//         return parseInt(Object.values(a.fares)[0]) - parseInt(Object.values(b.fares)[0])
+//     })
+// }
+// console.log(resultArr)
